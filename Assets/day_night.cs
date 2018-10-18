3b8e630d4a0417c3aeedec2c809441bb90b0b7e6
Ichiro <1027952@etusd.org>
2018-10-17 18:07:44 -0700

    added daylight cycle and working on a stary night sky

---
+++ Assets/day_night.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class day_night : MonoBehaviour
+{
+
+    public Material day;
+    public Material night;
+
+    void Start()
+    {
+        RenderSettings.skybox = day;
+    }
+    void Update()
+    {
+        transform.Rotate(new Vector3(-3 * Time.deltaTime, 0, 0), Space.World);
+        if (transform.rotation.x < -10 && transform.rotation.x > -170)
+        {
+            RenderSettings.skybox = night;
+        }
+        else{
+            RenderSettings.skybox = day;
+        }
+    }
+}
